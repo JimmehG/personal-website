6e69085da6c45d7d596dd71330c0693ea96c1691
James Gronowski <j.gronowski@gmail.com>
2024-02-14 15:16:18 +1100

    Updated experiences

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -111,10 +111,10 @@ const experiences = [
       iconBg: '#EEEEEE',
       date: 'Jan 2021 - Feb 2022',
       points: [
-         'Developing and maintaining web applications using React.js and other related technologies.',
-         'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-         'Implementing responsive design and ensuring cross-browser compatibility.',
-         'Participating in code reviews and providing constructive feedback to other developers.',
+         'My first taste of professional web development using C# and Angular.',
+         'Working with product managers and sales employees to release new features and products.',
+         'Experience with Octopus and TeamCity. Learned the basics of DevOps',
+         'Learning how to handle Live problems quickly and safely.',
       ],
    },
    {
@@ -124,10 +124,10 @@ const experiences = [
       iconBg: '#383E56',
       date: 'Jan 2022 - Jan 2023',
       points: [
-         'Developing and maintaining web applications using React.js and other related technologies.',
-         'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-         'Implementing responsive design and ensuring cross-browser compatibility.',
-         'Participating in code reviews and providing constructive feedback to other developers.',
+         'Worked in many varied working environments and was able to quickly switch up my skills to adjust.',
+         'Able to flex my communication skills by dealing directly with the clients.',
+         'Managing my work using sprints and different project management tools such as JIRA.',
+         'Utilising a wide variety of tech to suit the client\'s needs or their ongoing project\'s needs.',
       ],
    },
    {
@@ -137,10 +137,10 @@ const experiences = [
       iconBg: '#FFFFFF',
       date: 'Jan 2023 - Dec 2023',
       points: [
-         'Developing and maintaining web applications using React.js and other related technologies.',
-         'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-         'Implementing responsive design and ensuring cross-browser compatibility.',
-         'Participating in code reviews and providing constructive feedback to other developers.',
+         'Working with a cloud connected IoT device using AWS and an Ionic/Angular Android/iOS app.',
+         'Managing the tasks for my team using Sprint pland and Retrospectives, as well as organising workshops to discuss next steps.',
+         'Collaborating with managers and setting expectations for my team.',
+         'Introduced JIRA as well as implementing the DevOps to hook into the JIRA automation to make task tracking a breeze.',
       ],
    },
 ];
